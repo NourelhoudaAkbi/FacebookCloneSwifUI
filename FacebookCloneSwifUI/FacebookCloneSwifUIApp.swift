397653960bd89fdbd284d55f5ef757f47ac33a00
NourelhoudaAkbi <nourelhoudaakbi@gmail.com>
2024-09-18 01:28:33 +0100

    Initial Commit

---
+++ FacebookCloneSwifUI/FacebookCloneSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookCloneSwifUIApp.swift
+//  FacebookCloneSwifUI
+//
+//  Created by Nour el houda Akbi on 18/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct FacebookCloneSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FacebookCloneSwifUITests/FacebookCloneSwifUITests.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookCloneSwifUITests.swift
+//  FacebookCloneSwifUITests
+//
+//  Created by Nour el houda Akbi on 18/9/2024.
+//
+
+import Testing
+@testable import FacebookCloneSwifUI
+
+struct FacebookCloneSwifUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
